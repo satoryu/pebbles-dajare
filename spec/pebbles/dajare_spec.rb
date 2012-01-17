20ab1bd7b13faec63a9816c6bf9559966698c0f7
Tatsuya Sato <satoryu.1981@gmail.com>
2012-01-17 23:31:41 +0900

    Pebbles::Dajare.generate_dajare generates puns for a given string

--- spec/pebbles/dajare_spec.rb
+++ spec/pebbles/dajare_spec.rb
@@ -1,4 +1,14 @@
+# encoding: utf-8
 require 'pebbles/dajare'
 
 describe Pebbles::Dajare do
+  before do
+    GoogleSuggest.stub(:suggest_for).with('ます').and_return([
+      {'suggestion' => '益若つばさ'}
+    ])
+  end
+  
+  subject { Pebbles::Dajare.generate_dajare 'ありがとうございます' }
+  its(:shift) { should == 'ありがとうござい益若つばさ' }
+
 end
