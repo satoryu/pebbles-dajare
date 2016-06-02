d0891ec27c8204ac6c30a7e682f0f332fe44e736
Tatsuya Sato <satoryu.1981@gmail.com>
2016-06-02 18:32:23 +0900

    Accepts options for `dajarize` as hash (#3)
    * Use describe instead of context for grouping examples of each method
    
    * Use keyword arguments
    
    * Allows to specify how long of suffix of a given phrase is used for suggestion
    
    * Allows to specify the endpoint of Google Suggest API by giving a region code

--- spec/pebbles/dajare_spec.rb
+++ spec/pebbles/dajare_spec.rb
@@ -1,20 +1,37 @@
-# encoding: utf-8
 require 'pebbles/dajare'
 
 describe Pebbles::Dajare do
+  let(:options) { { region: 'jp' } }
+  let(:keyword) { 'ます' }
+
   before do
-    GoogleSuggest.stub(:suggest_for).with('ます').and_return([
+    GoogleSuggest.stub(:suggest_for).with(keyword, options).and_return([
       '舛添要一'
     ])
   end
 
-  context ".generate_dajare" do
+  describe ".generate_dajare" do
     subject { Pebbles::Dajare.generate_dajare 'ありがとうございます' }
 
     its(:shift) { should == 'ありがとうござい舛添要一' }
+
+    context 'Giving degree option' do
+      let(:keyword) { 'います' }
+
+      subject { Pebbles::Dajare.generate_dajare('ありがとうございます', degree: 3) }
+
+      its(:shift) { should == 'ありがとうござ舛添要一' }
+    end
+    context 'Giving region option' do
+      let(:options) { { region: 'us' } }
+
+      subject { Pebbles::Dajare.generate_dajare('ありがとうございます', region: 'us') }
+
+      its(:shift) { should == 'ありがとうござい舛添要一' }
+    end
   end
 
-  context "String#dajarize" do
+  describe "String#dajarize" do
     subject { 'ありがとうございます'.dajarize }
 
     its(:shift) { should == 'ありがとうござい舛添要一' }
