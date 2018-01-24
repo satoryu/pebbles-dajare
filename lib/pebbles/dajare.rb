10cb85a8e6aa4a03fbca068e88c6a1d723648fb1
Tatsuya Sato <satoryu.1981@gmail.com>
2018-01-25 07:31:07 +0900

    Cleaning codes

--- lib/pebbles/dajare.rb
+++ lib/pebbles/dajare.rb
@@ -1,13 +1,11 @@
 require 'google_suggest'
-require 'pebbles/dajare/version'
 
 module Pebbles
   module Dajare
-
     def generate_dajare(phrase, degree: 2, region: 'jp')
       components = phrase.scan(/./)
 
-      prefix = components[0..-(degree+1)].join
+      prefix = components[0..-(degree + 1)].join
       src_dajare = components[-degree..-1].join
 
       results = GoogleSuggest.suggest_for(src_dajare, region: region)
@@ -15,11 +13,12 @@ module Pebbles
       dajares = results.map do |result|
         prefix + result.split(' ').shift
       end
-      return dajares
+
+      dajares
     end
 
     def dajarize(degree: 2, region: 'jp')
-      Pebbles::Dajare.generate_dajare(self.to_s, degree: degree, region: region)
+      Pebbles::Dajare.generate_dajare(to_s, degree: degree, region: region)
     end
 
     module_function :generate_dajare
